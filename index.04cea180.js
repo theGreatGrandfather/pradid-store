a6fe1237929c6eb939e26b8a87ce0149d62d35db
theGreatGrandfather <139214883+theGreatGrandfather@users.noreply.github.com>
2023-07-29 18:28:27 +0000

    Deploying to gh-pages from @ theGreatGrandfather/pradid-store@fa59e5ea8516a38c322e0d321165e103e7dd3728 🚀

---
+++ index.04cea180.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".header__burger"),t=document.querySelector(".header__menu"),c=document.querySelector("body");e.addEventListener("click",(l=>{l.preventDefault(),console.log(l.target),l.target!==e&&l.target!==e.firstElementChild||(e.classList.toggle("active"),t.classList.toggle("active"),c.classList.toggle("lock"))}));
+//# sourceMappingURL=index.04cea180.js.map
---
+++ index.258cab45.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".header__burger"),t=document.querySelector(".header__menu"),c=document.querySelector("body");e.addEventListener("click",(function(r){r.preventDefault(),console.log(r.target),r.target!==e&&r.target!==e.firstElementChild||(e.classList.toggle("active"),t.classList.toggle("active"),c.classList.toggle("lock"))}))}();
+//# sourceMappingURL=index.258cab45.js.map

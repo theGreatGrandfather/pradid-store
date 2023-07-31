3c1f948a3ca3eab2da0f3551e59dc492862fd7f4
Praded3 <o.pradidenko@gmail.com>
2023-07-31 17:29:44 +0300

    mobile menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,25 +1,30 @@
 const burger = document.querySelector('.header__burger');
 const headerMenu = document.querySelector('.header__bottom');
-const headerContacts = document.querySelector('.header__contacts');
-
-
+// const headerContacts = document.querySelector('.header__contacts');
 const body = document.querySelector('body');
+const backdrop = document.querySelector('.header__bottom__backdrop ');
+
 
 const toggleMobileMenu = (e)=>{
     e.preventDefault();
-    console.log(e.target)
     if (e.target === burger || e.target === burger.firstElementChild ) {
         burger.classList.toggle('active');
         headerMenu.classList.toggle('active');
-        headerContacts.classList.toggle('active');
+        // headerContacts.classList.toggle('active');
         body.classList.toggle('lock');
+        backdrop.classList.toggle('is-hidden');
+        backdrop.addEventListener('click', onBackdropClick);
     }
 };
 
 
-
-
-
-
-
+const onBackdropClick = (e) => {
+    e.preventDefault();
+    burger.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+    // headerContacts.classList.toggle('active');
+    body.classList.toggle('lock');
+    backdrop.classList.toggle('is-hidden');
+    backdrop.removeEventListener('click', onBackdropClick);
+}
 burger.addEventListener('click', toggleMobileMenu);
